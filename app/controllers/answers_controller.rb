325e9add5563b49f01b74b9c6d506b3f71f072b7
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2019-06-29 10:49:45 +0300

    refactor: remove except from authenticate_user

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,5 @@
 class AnswersController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!
   expose :question
   expose :answers, ->{ question.answers }
   expose :answer, build: ->{ answers.new(answer_params) }
