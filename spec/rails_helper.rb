d882744ef6c48520c92bcd6a0c97e4f075fda774
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2019-07-03 21:15:09 +0300

    chore: add RSpec negated matcher

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,9 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+RSpec::Matchers.define_negated_matcher :not_change, :change
+
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
