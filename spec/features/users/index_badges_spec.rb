0e2be233d662b77a2198b05f78cc473987928940
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2019-07-14 12:28:43 +0300

    test: add test for unauthenticated user in index badges

--- spec/features/users/index_badges_spec.rb
+++ spec/features/users/index_badges_spec.rb
@@ -7,7 +7,13 @@ feature 'The user can look through the badges received by him', %q{
   given(:user) { create(:user) }
   given!(:question) { create(:question, user: user) }
   given!(:badge) { create(:badge, question: question, user: user) }
+
   describe 'Unauthenticated user' do
+    scenario 'tries to see badges' do
+      visit badges_index_path
+
+      expect(page).to have_content('You need to sign in or sign up before continuing.')
+    end
   end
 
   describe 'Authenticated user' do
