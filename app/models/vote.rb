f5749ea570c6bea23294d514185887cb1d74de42
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2019-07-17 13:54:55 +0300

    refactor: add uniqueness validation for votable

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,5 @@ class Vote < ApplicationRecord
   belongs_to :user
 
   validates :vote_for, presence: true, inclusion: [-1, 1]
+  validates :user, uniqueness: { scope: %i[votable_id votable_type] }
 end
