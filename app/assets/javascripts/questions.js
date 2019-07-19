d867f5745c55fbe235c7f4eb4faf012fc19ef64b
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2019-07-19 22:26:01 +0300

    refactor: replace .hidden by .d-none

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -2,6 +2,6 @@ $(document).on('turbolinks:load', function() {
   $('.question').on('click', '.edit-question-link', function(e) {
     e.preventDefault();
     $(this).hide();
-    $('.question').find('.form').removeClass('hidden');
+    $('.question').find('.form').removeClass('d-none');
   })
 });
