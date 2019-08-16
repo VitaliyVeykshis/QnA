9eb1c78e24f4d2d93ffe7ef5c7d47089ca74ccfa
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2019-08-16 12:09:11 +0300

    chore: delete helper Controllers::Authentication

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -46,7 +46,6 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Helpers::Features::Authentication, type: :feature
-  config.include Helpers::Controllers::Authentication, type: :controller
   config.include Rails.application.routes.url_helpers
   config.include RedirectBack
   config.include Helpers::ApiHelpers, type: :request
