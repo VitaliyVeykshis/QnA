39911978f3eb0a932b98948e64ddd36ddc2e025d
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2019-08-22 12:19:42 +0300

    refactor: change comment view

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,6 +6,8 @@ class Comment < ApplicationRecord
 
   validates :body, presence: true
 
+  scope :persisted, -> { select(&:persisted?) }
+
   def search_result
     { title: commentable.search_result[:title], body: body[0..100], link_to: commentable }
   end
