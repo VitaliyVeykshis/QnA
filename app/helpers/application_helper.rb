c7d491ef8fcde8e0ccbc18d11893143f01190038
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2019-08-22 13:41:51 +0300

    feat: add flash message view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  FLASH_STYLES = {
+    alert: 'danger',
+    notice: 'success'
+  }.freeze
+
   def search_categories
     {
       all: 'All',
@@ -7,5 +12,9 @@ module ApplicationHelper
       comment: 'Comments',
       user: 'Users'
     }
+  end
+
+  def flash_style(type)
+    "alert alert-#{FLASH_STYLES[type.to_sym]}"
   end
 end
